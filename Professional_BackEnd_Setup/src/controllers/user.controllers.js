68195f81748ccca9df22dedd83950ce74ee7004d
SaimRazaAly <saimrazateach2010@gmail.com>
2024-12-16 16:34:41 -0800

    some add in cnotrollers

--- Professional_BackEnd_Setup/src/controllers/user.controllers.js
+++ Professional_BackEnd_Setup/src/controllers/user.controllers.js
@@ -1,9 +1,37 @@
 import { asyncHandler } from "../utils/asyncHandlers.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiError.js";
+import  {User} from "../models/user.model.js"
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "saim has sucessfuly done the userRegisteratoin",
-  });
+  // ALGORITHM  for registerting user
+
+  // get details from frontend
+  const { username, email, fullName, password } = req.body;
+  console.log("user name :", username,password);
+
+  // validation of
+
+  if ( [username, email, fullName, password, avatar].some(field => field?.trim() === "")
+) {
+    throw new ApiError(400,"All fields are requried");
+    
+  }
+  // email or password alerady exists
+const aleradyExist = User.findOne({
+  $or: [{email},{username}]
+}) 
+
+if(aleradyExist){
+  throw new ApiError(409,"user with enail or username already exits")
+}
+req.files.avatar[0]?.path
+
+  // check for images ,avatar
+  // upload the images to cloudniary
+  // createa a new entery in DB
+  // remove password and refresh token form feilds
+  // cheack for user creation
+  // return res
 });
 
-
-export default registerUser
+export default registerUser;
